04865b2135dd28bec3d0ac99fb8a37d5cdc8eb8e
Trevor Brown <admin@stratus3d.com>
2017-07-15 16:18:40 -0400

    Complete the process_chain conversion to Erlang.

---
+++ process_chain/README.md
@@ -0,0 +1,34 @@
+process_chain
+=============
+
+## Description
+
+Thousands of processes arranged in a loop passing messages around the loop. Demonstrates the efficiency of the Erlang VM’s ligtweight process creation and message passing.
+
+## Dependencies
+
+Erlang 19.2
+Rebar 3.4.1
+
+## Usage
+
+Start up an erl session:
+
+    rebar3 shell
+
+Then start a loop in the iex session:
+
+    > Processes = 100 % Number of processes in chain
+    > Loops = 10 % Number of times the message should be sent around the chain
+    > Message = test % The messsage to send
+    > process_chain:start(Processes, Loops, Message)
+
+`start_timed` does the same thing but times how long the looping function takes:
+
+    > process_chain:start_timed(Processes, Loops, Message)
+
+You should see something like this printed out after you run the start function:
+
+    Sending message `test`!
+    Got message `test`!
+    ok
--- process_chain/src/chain_controller.erl
+++ process_chain/src/chain_controller.erl
@@ -7,7 +7,7 @@ start(Loops, Message, Timed) ->
   case Timed of
     true ->
       {Microseconds, ok} = timer:tc(?MODULE, wait, [Loops, Message]),
-      io:format("~w Loops completed in ~w seconds", [Loops, (Microseconds / 1000000)]);
+      io:format("~w Loops completed in ~w seconds~n", [Loops, (Microseconds / 1000000)]);
     false ->
       wait(Loops, Message)
   end.
@@ -16,7 +16,7 @@ wait(Loops, Message) ->
   receive
     {start, FirstLink} ->
       % Start the first loop
-      io:format("Sending Message `~p`!", [Message]),
+      io:format("Sending Message `~p`!~n", [Message]),
       FirstLink ! Message,
       loop(Loops-1, Message, FirstLink)
   end.
@@ -24,7 +24,7 @@ wait(Loops, Message) ->
 loop(0, Message, FirstLink) ->
   % All loops complete, tell the chain link processes to shutdown
   FirstLink ! shutdown,
-  io:format("Got Message `~p`!", [Message]);
+  io:format("Got Message `~p`!~n", [Message]);
 loop(Loops, Message, FirstLink) ->
   receive
     Message ->
